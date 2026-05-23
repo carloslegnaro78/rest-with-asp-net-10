76f5ac91ee233b06608fd03e397360149fbab8ec
carlos.legnaro <carlos.legnaro@outlook.com>
2026-05-23 14:52:33 -0300

    Implementando as Classes Controller e Model

---
+++ RestWithASPNET10/Controllers/PersonController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET10.Model;
+
+
+namespace RestWithASPNET10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+
+    }
+}
