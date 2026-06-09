a0d9bc55c0f455861d144607e0a45544d14e87bf
carloslegnaro78 <carlos_legnaro@outlook.com>
2026-06-09 13:57:09 -0300

    Finalizando a Implementação

--- RestWithASPNET10/Program.cs
+++ RestWithASPNET10/Program.cs
@@ -1,4 +1,5 @@
 using RestWithASPNET10.Configurations;
+using RestWithASPNET10.Repositories.Impl;
 using RestWithASPNET10.Services;
 using RestWithASPNET10.Services.Impl;
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddDatabaseConfiguration(builder.Configuration);
 builder.Services.AddScoped<IPersonServices, PersonServicesImpl>();
+builder.Services.AddScoped<IPersonRepository, IPersonRepository>();
 
 var app = builder.Build();
 
